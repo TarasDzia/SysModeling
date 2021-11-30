6e0f029e8f29e57244458405b80fc281fd12b8f3
TarasDzia <73058218+TarasDzia@users.noreply.github.com>
2021-11-30 13:53:22 +0200

    reimport lab2 files

---
+++ src/main/java/lpnu/sys_modeling/labs/lab2/app.java
@@ -0,0 +1,24 @@
+package lpnu.sys_modeling.labs.lab2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class app extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindowController.class.getResource("main_window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 944, 600);
+        stage.setTitle("lab2");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
-module lpnu.sys_modeling.labs.lab3s{
+module lpnu.sys_modeling.labs{
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
 
     opens lpnu.sys_modeling.labs.lab3 to javafx.fxml;
     exports lpnu.sys_modeling.labs.lab3;
+    opens lpnu.sys_modeling.labs.lab2 to javafx.fxml;
+    exports lpnu.sys_modeling.labs.lab2;
+    opens lpnu.sys_modeling.labs.lab2.objects to javafx.fxml;
+    exports lpnu.sys_modeling.labs.lab2.objects;
+    opens lpnu.sys_modeling.labs.lab2.threads to javafx.fxml;
+    exports lpnu.sys_modeling.labs.lab2.threads;
 }
